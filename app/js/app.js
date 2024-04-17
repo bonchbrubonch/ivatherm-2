3fb64602ffb3586bdabcdd6fe049178b08a9105d
Artem <nikolloteslla@gmail.com>
2024-04-17 11:18:26 +0300

    add remark

--- app/js/app.js
+++ app/js/app.js
@@ -90,8 +90,8 @@ $(function () {
         var container = $(this).parents('.tabbed-content'),
           currId = $(this).attr('id'),
           items = container.find('.item');
-        // container.find('.tabs a').removeClass('active');
-        // items.removeClass('active');
+        container.find('.tabs a').removeClass('active');
+        items.removeClass('active');
         $(this).toggleClass('active');
         container.find('.tabs a[href$="#' + currId + '"]').addClass('active');
       });
